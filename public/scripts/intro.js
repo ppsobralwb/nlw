a9c2823129dbc8efb141bec1a95b4361efa7be03
Pedro Paulo <ppsobralwb@gmail.com>
2020-06-11 13:00:29 -0300

    public

---
+++ public/scripts/intro.js
@@ -0,0 +1,81 @@
+// ->coments
+// ------------------------------------------------------------------
+// document.write("Hello World")
+// ------------------------------------------------------------------
+// variaveis, tipos de dados
+// let myvar = "Hello"
+// var my = "Hey"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+// -----------------------------------------------------------------
+// string
+// const s1 = "String"
+// const s2 = 'String'
+// const s3 = `String`
+
+// document.write(s3)
+// -----------------------------------------------------------------
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+// -----------------------------------------------------------------
+// boolean - true or false
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue + bTrue)
+// ----------------------------------------------------------------
+// objetos possuem prpriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return "Run Forest"
+//   }
+// }
+
+// document.write(pessoa.correr() + pessoa.correr())
+
+// const arquivo = {
+//   nome: "nulo",
+//   data: "04/06/2020",
+//   teste(){
+//     document.write("testando")
+//   }
+// }
+
+// arquivo.teste()
+// -----------------------------------------------------------------
+// Array - Vetores (coleção de dados)
+// const colecaodebolinhas = ["blue", "green", 1, {name: "my name"}]
+
+// document.write(colecaodebolinhas[3].name)
+// -----------------------------------------------------------------
+// Funçoes - Funcionalidades - Atalhos - Reuso de código
+// function sayMyName(name){
+//   document.write(name)
+// }
+// // executar
+// sayMyName("Maghda")
+// sayMyName("Paulo")
+// sayMyName("Pedro")
+// -----------------------------------------------------------------
+// condicionais
+// const notaFinal = 7
+// if ( notaFinal < 5) {
+//   document.write("Reprovado")
+// } else{
+//   document.write("Aprovado")
+// }
+// -----------------------------------------------------------------
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write("<p> dale </p>")
+// }
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
